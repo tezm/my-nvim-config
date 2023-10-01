ddd61cdfbfbb28e4e03aa69aa68de39b88b2f4d0
Gte1m <kawakasta@gmail.com>
2023-10-01 21:41:04 +0200

    Add formater

---
+++ lua/mnat/plugins/conform.lua
@@ -0,0 +1,27 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        markdown = { "prettier" },
+        lua = { "stylua" },
+        ocaml = { "ocamlformat" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        timeout_ms = 1000,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+      conform.format({
+        lsp_fallback = true,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+}
