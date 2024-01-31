d7221fe263540a37bff8a8710ddfeff549b13ec3
Gte1m <kawakasta@gmail.com>
2024-01-31 02:02:49 +0100

    Added more fluid navigatgeion in tmux

---
+++ lua/mnat/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,22 @@
+return {
+  "alexghergh/nvim-tmux-navigation",
+  cmd = {
+    "NvimTmuxNavigateLeft",
+    "NvimTmuxNavigateDown",
+    "NvimTmuxNavigateUp",
+    "NvimTmuxNavigateRight",
+    "NvimTmuxNavigateLastActive",
+    "NvimTmuxNavigateNext",
+  },
+  opts = {
+    disable_when_zoomed = true -- defaults to false
+  },
+  keys = {
+    { "<C-h>",     "<cmd>NvimTmuxNavigateLeft<cr>",       mode = "n", desc = "Navigate left split" },
+    { "<C-j>",     "<cmd>NvimTmuxNavigateDown<cr>",       mode = "n", desc = "Navigate left split" },
+    { "<C-k>",     "<cmd>NvimTmuxNavigateUp<cr>",         mode = "n", desc = "Navigate left split" },
+    { "<C-l>",     "<cmd>NvimTmuxNavigateRight<cr>",      mode = "n", desc = "Navigate left split" },
+    { "<C-\\>",    "<cmd>NvimTmuxNavigateLastActive<cr>", mode = "n", desc = "Navigate left split" },
+    { "<C-Space>", "<cmd>NvimTmuxNavigateNext<cr>",       mode = "n", desc = "Navigate left split" },
+  },
+}
